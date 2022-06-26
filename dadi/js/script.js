6861f5c823da978b07e24fa78ebe654fb6ad01bd
Eikcaj.1998 <jw220998@gmail.com>
2022-06-26 14:03:37 +0200

    base-dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,5 @@
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilisci il vincitore, in base a chi fa il punteggio più alto e stampa  in console il risultato (ossia se ha vinto il computer o il player)
+*/
+console.log('JS OK');
